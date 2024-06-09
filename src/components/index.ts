a2eb43236b35762964ea019d4afa9160cece760d
geraldkachi <fitzgeraldkachi@gmail.com>
2024-06-09 08:00:56 +0100

    upda

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,3 @@
-export {default as NavBar} from './Navbar'
 export {default as Button} from './button/Button'
 export {default as Hero} from './Hero'
 export {default as Accordion} from './Accordion/Accordion'
